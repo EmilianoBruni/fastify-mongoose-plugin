ebc8e0be28eb6245f516e5c88dfa8f5274b749d1
Emiliano Bruni <info@ebruni.it>
2024-02-04 16:03:28 +0100

    Partial definition of lib types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,13 @@
-import { FastifyPluginOptions } from "fastify";
+import type { FastifyPluginOptions } from 'fastify';
+import type { MongooseOptions, SchemaTypeOptions } from 'mongoose';
 
-export type TFMPOptions = FastifyPluginOptions & {
-    // TODO: Add your plugin options here
+export type TFMPModel<T = any> = SchemaTypeOptions<T>;
+export type TFMPModels<T = any> = Array<TFMPModel<T>>;
+
+export type TFMPOptions<T = any> = FastifyPluginOptions & {
+    uri: string;
+    settings?: MongooseOptions;
+    models?: TFMPModels<T>;
+    useNameAndAlias?: boolean;
+    modelDirPath?: string;
 };
