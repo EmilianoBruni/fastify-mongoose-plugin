e6bf18e732c964762dce07d2f1587fe0d251ac4d
Emiliano Bruni <info@ebruni.it>
2024-02-04 20:49:16 +0100

    Add mongoose import and update TFMPPlugin interface

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,31 @@
 import type { FastifyPluginOptions } from 'fastify';
 import type { MongooseOptions, SchemaTypeOptions } from 'mongoose';
+import type mongoose from 'mongoose';
 
-export type TFMPModel<T = any> = SchemaTypeOptions<T>;
+declare module 'fastify' {
+    export interface FastifyInstance {
+        mongoose: TFMPPlugin;
+    }
+}
+
+export type TFMPPlugin = {
+    [key: string]: typeof mongoose.Model;
+} & {
+    instance: typeof mongoose;
+};
+
+export type TFMPSchema<T = any> = SchemaTypeOptions<T>;
+
+export type TFMPModel<T = any> = {
+    name: string;
+    alias?: string;
+    schema: TFMPSchema<T>;
+    options?: any;
+    class?: any;
+    virtualize?: (schema: any) => void;
+};
+
+// export type TFMPModel<T = any> = SchemaTypeOptions<T>;
 export type TFMPModels<T = any> = Array<TFMPModel<T>>;
 
 export type TFMPOptions<T = any> = FastifyPluginOptions & {
